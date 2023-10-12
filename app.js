e205764121c595e51f9b5c7d73ad099899446294
Marlon-Somers <9021883@student.zadkine.nl>
2023-10-12 09:34:41 +0200

    opdracht genaakt

---
+++ app.js
@@ -0,0 +1,10 @@
+
+    function kleur_aanpassen(){
+        document.getElementById("inhoud").style.color = "blue"
+        document.getElementById("inhoud").innerHTML = "inhoud aangepast!"
+    }
+    kleur_aanpassen()
+   
+    
+
+ 
